750d5990fcd6891e9cfe1805bad4cbf96818e942
Ignacio Ojeda <vvalentinojeda18@gmail.com>
2025-06-03 01:45:00 -0300

    actualizando fondo y menu

--- miniproyecto.py
+++ miniproyecto.py
@@ -1,31 +1,36 @@
 import tkinter as tk
 
 #Config. de ventana
-ventana = tk.Tk()
-ventana.geometry("600x600")
-ventana.config(bg="NavajoWhite2")
-ventana.attributes("-alpha",0.95)
-ventana.title("Proyecto")
-ventana.iconbitmap("Assets\Carrito.ico")
+ventana_inicio = tk.Tk()
+ventana_inicio.geometry("600x600")
+ventana_inicio.attributes("-alpha",0.95)
+ventana_inicio.title("Proyecto")
+ventana_inicio.iconbitmap("Assets/Carrito.ico")
+Imagen = tk.PhotoImage(file="Assets/Fondo.png")
+Fondo = tk.Label(ventana_inicio, image=Imagen)
+Fondo.place(x=0, y=0, relwidth=1, relheight=1)
 
-menu_barra = tk.Menu(ventana)
+#Config. del menu
+menu_barra = tk.Menu(ventana_inicio)
 
+Inicio = tk.Menu(menu_barra, tearoff=0)
+Inicio.add_command(label="Venta")
+Inicio.add_command(label="Salir", command=ventana_inicio.destroy)
 
-
-submenu = tk.Menu(menu_barra, tearoff=0)
-submenu.add_command(label="Carrito")
-submenu.add_command(label="Ventas")
-submenu.add_separator()
-
-Opciones_menu = tk.Menu(submenu, tearoff=0)
-Opciones_menu.add_command(label="Apariencia")
-Opciones_menu.add_command(label="Contacto")
-
-submenu.add_cascade(label="Opciones", menu=Opciones_menu)
+Stock = tk.Menu(menu_barra, tearoff=0)
 
 
-menu_barra.add_cascade(label="Principal", menu=submenu)
+Ayuda = tk.Menu(menu_barra, tearoff=0)
+Ayuda.add_command(label="Contacto")
+Ayuda.add_command(label="Sobre nosotros")
 
-ventana.config(menu=menu_barra)
 
-ventana.mainloop()
+
+menu_barra.add_cascade(label="Inicio", menu=Inicio)
+menu_barra.add_cascade(label="Stock", menu=Stock)
+menu_barra.add_cascade(label="Ayuda", menu=Ayuda)
+
+ventana_inicio.config(menu=menu_barra)
+
+
+ventana_inicio.mainloop()
